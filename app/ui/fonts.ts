c62f5b39394ec14d83592308168b03b5ced2420e
Vito <vitodipinto@outlook.com>
2024-06-21 06:42:13 +0100

    commit repository

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({
+  subsets: ['latin'],
+})
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400', // specify the font weight here, e.g., '400' for normal weight or '700' for bold
+})
